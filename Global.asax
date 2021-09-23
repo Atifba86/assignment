4717393c2b5e335d28d83f4230c8051749ee9abe
Rao Atif <70482315+Atifba86@users.noreply.github.com>
2021-09-23 23:19:53 +0500

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SearchFunctionalityWithJquery.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SearchFunctionalityWithJquery.Startup))]
+namespace SearchFunctionalityWithJquery
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
