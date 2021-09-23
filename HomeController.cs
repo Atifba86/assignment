f23cc29f0c61afb334f51ade8686627241812eb9
Rao Atif <70482315+Atifba86@users.noreply.github.com>
2021-09-23 23:25:53 +0500

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,42 @@
+﻿using SearchFunctionalityWithJquery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SearchFunctionalityWithJquery.Controllers
+{
+    public class HomeController : Controller
+    {
+        MVCTutorialEntities db = new MVCTutorialEntities();
+        public ActionResult Index()
+        {
+            return View(db.StudentInfoes.ToList());
+        }
+
+        public JsonResult GetSearchingData(string SearchBy, string SearchValue)
+        {
+            List<StudentInfo> StuList = new List<StudentInfo>();
+            if (SearchBy == "ID")
+            {
+                try
+                {
+                    int Id = Convert.ToInt32(SearchValue);
+                    StuList = db.StudentInfoes.Where(x => x.StuId == Id || SearchValue == null).ToList();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("{0} Is Not A ID ", SearchValue);
+                }
+                return Json(StuList, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                StuList = db.StudentInfoes.Where(x => x.StuName.StartsWith(SearchValue) || SearchValue == null).ToList();
+                return Json(StuList, JsonRequestBehavior.AllowGet);
+            }
+        }
+        
+    }
+}
